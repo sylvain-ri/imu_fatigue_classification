a9bcfdb2961892274a9513fa4d4f19eb06eb93eb
Sylvain R <sylvainriondet@gmail.com>
2018-04-18 17:03:12 +0800

    Identifying and labelling done next step fft

--- plot_sensors.py
+++ plot_sensors.py
@@ -2,10 +2,11 @@
 # Imports
 import logging
 import os
+import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import label
 from matplotlib import pyplot as plt
-import pandas as pd
+from numpy.fft import fft
 from pprint import pprint
 
 
@@ -13,10 +14,10 @@ from pprint import pprint
 # #######################################################################
 # #######################       Parameters        #######################
 # #######################################################################
-DEBUG = 2
+DEBUG = 3
 SKIP_BEG_SEC = 1        # remove what happens when the recording on the smartphone is started and put into pocket
-SKIP_END_SEC = 2        # same, for the end, when removed from pocket
-SKIP_CHANGE_SEC = 2     # same, between transitions
+SKIP_END_SEC = 1        # same, for the end, when removed from pocket
+SKIP_CHANGE_SEC = 1     # same, between transitions
 THRESHOLD = 1          # threshold to cut the data
 LABELS = {"none": -1, "standing": 0,
           "walk_rested": 1, "walk_tired": 2, "walk_very_tired": 3}
@@ -28,7 +29,7 @@ LABELS = {"none": -1, "standing": 0,
 def load_csv(file_name):
     # Get read of first line (sep=;) when reading the csv file
     data = pd.read_csv(file_name, sep=";", skiprows=1, index_col=None)
-    data["label"] = LABELS["none"]
+    data["labels"] = LABELS["none"]
     return data
 
 
@@ -38,7 +39,7 @@ def load_csv(file_name):
 def clean_data(data, tiredness_state):
     # Set the datetime as index
     data.date_time = pd.to_datetime(data["date_time"], format='%Y-%m-%d %H:%M:%S:%f')
-    data.set_index("date_time")
+    # data.set_index("date_time")
     # time_ms converted into seconds
     data.time_ms = data.time_ms / 1000
     # find total time, sampling frequency
@@ -54,20 +55,20 @@ def clean_data(data, tiredness_state):
     # #######################################################################
     logging.info("Printing section")
     # Print some stuff
-    if DEBUG > 3:
+    if DEBUG > 6:
         print(data.head())
         print(data.columns)
         print(data.dtypes)
 
 
     # Plotting
-    if DEBUG > 5:
+    if DEBUG > 3:
         columns = ["acc_x", "acc_y", "acc_z", "grav_x", "grav_y", "grav_z",
                    "lin_acc_x", "lin_acc_y", "lin_acc_z",
                    "gyro_x", "gyro_y", "gyro_z",
                    "mag_x", "mag_y", "mag_z", "sound_lvl", ]
-        col_to_plot = ["acc_x", "acc_y", "acc_z"]
-        plt.figure(f"{index}- All columns")
+        col_to_plot = ["acc_x", "acc_y", "acc_z", "gyro_x", "gyro_y", "gyro_z", "mag_x", "mag_y", "mag_z", ]
+        plt.figure(f"{index}- ROW data")
         plt.plot(data.time_ms, data[col_to_plot])
         plt.legend(col_to_plot)
         plt.tight_layout()
@@ -84,7 +85,7 @@ def clean_data(data, tiredness_state):
     # add each difference on a 0.2 sec window
     # if that sum of noise is low, cut before, define it as standing state.
     # If DEBUG
-    if DEBUG > 3:
+    if DEBUG > 5:
         plt.figure(f"{index}- Smooth and acc variation")
 
     # Smoothing each acc axis
@@ -106,7 +107,7 @@ def clean_data(data, tiredness_state):
 
 
     # If DEBUG
-    if DEBUG > 3:
+    if DEBUG > 5:
         plt.plot(data.time_ms, data["acc_sum"], label="acc_sum")
         plt.plot(data.time_ms, data["acc_sum_smoth"], label="acc_sum_smoth")
         plt.tight_layout()
@@ -115,21 +116,48 @@ def clean_data(data, tiredness_state):
 
     #
     # ##################################################################
-    # todo skip beginning and end of files
-    # min_variation = data["acc_sum_smoth"].min
-    data.drop(range(data.shape[0] - SKIP_END_SEC*frequency, data.shape[0]), inplace=True)
+    # skip beginning and end of files
+    # Labels
+    # ##################################################################
     data.drop(range(SKIP_BEG_SEC*frequency), inplace=True)
 
     # Ensure that we found the resting state
-    ind_start = data[data["acc_sum_smoth"] < THRESHOLD].index
-    data.drop(data.index[range(ind_start[0])], inplace=True)      # remove beginning with noise
-    ind_start_walk = data[data["acc_sum_smoth"] > THRESHOLD].index[0]
-    data["label"].iloc[0:ind_start_walk] = LABELS[tiredness_state]
+    i0_set_rest = data[data["acc_sum_smoth"] < THRESHOLD].index[0]
+    data.drop(data.index[range(i0_set_rest)], inplace=True)      # remove beginning with noise
+    i0_set_rest = 0
+    # Label until the end of first rest
+    i1_rest_walk = data.loc[data.index > i0_set_rest + SKIP_CHANGE_SEC*frequency, "acc_sum_smoth"][data["acc_sum_smoth"] > THRESHOLD].index[0]
+    data.loc[data.index <= i1_rest_walk, "labels"] = LABELS["standing"]
+    # Label until the end of first walking
+    i2_walk_rest = data.loc[data.index > i1_rest_walk + SKIP_CHANGE_SEC*frequency, "acc_sum_smoth"][data["acc_sum_smoth"] < THRESHOLD].index[0]
+    data.loc[(i1_rest_walk <= data.index) & (data.index < i2_walk_rest), "labels"] = LABELS[tiredness_state]
+    # Label until the turn
+    i3_rest_turn = data.loc[data.index > i2_walk_rest + SKIP_CHANGE_SEC*frequency, "acc_sum_smoth"][data["acc_sum_smoth"] > THRESHOLD].index[0]
+    data.loc[(i2_walk_rest <= data.index) & (data.index < i3_rest_turn), "labels"] = LABELS["standing"]
+    # Label until rest
+    i4_turn_rest = data.loc[data.index > i3_rest_turn + SKIP_CHANGE_SEC*frequency, "acc_sum_smoth"][data["acc_sum_smoth"] < THRESHOLD].index[0]
+    data.loc[(i3_rest_turn <= data.index) & (data.index < i4_turn_rest), "labels"] = LABELS["none"]
+    # Label until 2nd walk
+    i5_rest_walk = data.loc[data.index > i4_turn_rest + SKIP_CHANGE_SEC*frequency, "acc_sum_smoth"][data["acc_sum_smoth"] > THRESHOLD].index[0]
+    data.loc[(i4_turn_rest <= data.index) & (data.index < i5_rest_walk), "labels"] = LABELS["standing"]
+    # Label until 2nd rest
+    i6_walk_rest = data.loc[data.index > i5_rest_walk + SKIP_CHANGE_SEC*frequency, "acc_sum_smoth"][data["acc_sum_smoth"] < THRESHOLD].index[0]
+    data.loc[(i5_rest_walk <= data.index) & (data.index < i6_walk_rest), "labels"] = LABELS[tiredness_state]
+    # Label until noise
+    i7_rest_noise = data.loc[data.index > i6_walk_rest + SKIP_CHANGE_SEC*frequency, "acc_sum_smoth"][data["acc_sum_smoth"] > THRESHOLD].index[0]
+    data.loc[(i6_walk_rest <= data.index) & (data.index < i7_rest_noise), "labels"] = LABELS["standing"]
+    # Drop end part
+    data.drop(data.loc[(i7_rest_noise <= data.index)].index, inplace=True)
 
-    # data.drop(data.tail(len(data) - ind_low_noise[-1]).index, inplace=True)     # removes end with noise
 
-    print(ind_start, ) # ind_start_walk
-    print(data["label"].sample(10))
+    # Collect indexes
+    split_indexes = [i0_set_rest, i1_rest_walk, i2_walk_rest, i3_rest_turn, i4_turn_rest,
+                     i5_rest_walk, i6_walk_rest, i7_rest_noise]
+
+    if DEBUG > 1:
+        print(split_indexes)
+    if DEBUG > 5:
+        print(data["labels"].sample(10))
 
 
 
@@ -145,10 +173,11 @@ def clean_data(data, tiredness_state):
         plt.figure(f"{index}- Sum of variation")
         plt.plot(data.time_ms, data["acc_sum"], label="acc_sum")
         plt.plot(data.time_ms, data["acc_sum_smoth"], label="acc_sum_smoth")
+        plt.plot(data.time_ms, data["labels"], label="labels")
         plt.tight_layout()
         plt.legend()
 
-    return
+    return split_indexes
 
 
 
@@ -206,25 +235,30 @@ os.chdir(folder)
 files = [f for f in os.listdir(folder) if f.endswith(".csv")]
 logging.info(f"found {len(files)} files in folder {folder}")
 
-
 # #######################################################################
 # #######################          MAIN           #######################
 # #######################################################################
-for index, file_name in enumerate([files[5], files[17]]):
+# for index, file_name in enumerate([files[5], files[17]]):
+for index, file_name in enumerate(files):
 
-    state = "none"
-    if index in range(0, 6):      state = "walk_rested"
-    elif index in range(6, 12):   state = "walk_tired"
-    elif index in range(12, 18):  state = "walk_very_tired"
+    if index in (5, 17):
+        state = "none"
+        if index in range(0, 6):      state = "walk_rested"
+        elif index in range(6, 12):   state = "walk_tired"
+        elif index in range(12, 18):  state = "walk_very_tired"
 
-    print(f"*** File number {index} ***")
-    data = load_csv(file_name)
-    print("data loaded")
-    clean_data(data, state)
-    print("data cleaned \n")
+        print(f"\n *** File number {index} ***")
+        data = load_csv(file_name)
+        print("data loaded")
+        print(data.shape)
+        split_indexes = clean_data(data, state)
+        print(data.shape)
+        print("data cleaned \n")
 
+# whyyyyyy is plt. blocking...
 plt.show()
-
+input("press any key: ")
+plt.close("all")
 
 
 
