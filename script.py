43b14abcf5af44ccb2c0e84943f30b13c4720889
Sylvain R <sylvainriondet@gmail.com>
2018-05-07 18:33:01 +0800

    simplified the call from console

--- script.py
+++ script.py
@@ -10,12 +10,20 @@ if False:
     print(s)
 
 # How to call the main script from the console
+import os
+os.chdir("D:/Drive/Singapore/Courses/CS6206-HCI Human Computer Interaction/Project/Python")
 from plot_sensors import *
 
 files = setup_files()
 files_load_clean_to_feat(files)
 fft_frames()
 
-one_frame = all_frames[5]['frame']
+# one_frame = all_frames[5]['frame']
+
+plot_fft(8)
+
+
+
+# plt.semilogy(xf[1:N//2], 2.0/N * np.abs(signal[1:N//2]), '-b')
 
 
